a6515c7985da737559fc690bf98cab666418a9a2
exit <tish@email.com>
2023-10-26 14:23:34 -0400

    Intial Commit Character Class

---
+++ LevelUpGame.Tests/levelup/CharacterTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using levelup;
+
+namespace levelup
+{
+    [TestFixture]
+    public class CharacterTest
+    {
+        private Character? testObj;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testObj = new Character();
+        }
+
+
+        
+    }
+
+
+}

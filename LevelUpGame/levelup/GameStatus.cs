51be0e4355e3f53351001cb28d03aa7e6d9f8a69
steve bergerson <scbergerson@gmail.com>
2023-10-27 11:38:36 -0400

    created GameStatus code

---
+++ LevelUpGame/levelup/GameStatus.cs
@@ -0,0 +1,21 @@
+using System.Drawing;
+
+namespace levelup
+{
+    public class GameStatus
+    {
+        public readonly string characterName;
+
+        public Point currentPosition;
+
+        public int moveCount;
+
+        public string toString()
+        {
+            return "";
+        }
+
+    }
+
+
+}
